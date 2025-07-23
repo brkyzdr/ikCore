b59df0fd20d9cc2410e506dc4d957daa759f344d
Berkay Ozder <berkayq3535@gmail.com>
2025-07-23 16:56:49 +0300

    Unit Of Work

---
+++ ikCore/DataAccess/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using ikCore.DataAccess.Repositories.Interfaces;
+
+namespace ikCore.DataAccess.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICompanyRepository Companies { get; }
+        int Complete();
+    }
+}
---
+++ ikCore/DataAccess/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using ikCore.DataAccess.Context;
+using ikCore.DataAccess.Repositories.Concrete;
+using ikCore.DataAccess.Repositories.Interfaces;
+
+namespace ikCore.DataAccess.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+            Companies = new CompanyRepository(_context);
+        }
+
+        public ICompanyRepository Companies { get; private set; }
+
+        public int Complete() => _context.SaveChanges();
+
+        public void Dispose() => _context.Dispose();
+    }
+}
