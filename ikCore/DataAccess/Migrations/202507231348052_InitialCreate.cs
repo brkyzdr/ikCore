e9b34cb727c777a613597d0b2df41172b5ea85cd
Berkay Ozder <berkayq3535@gmail.com>
2025-07-23 16:50:46 +0300

    Initial Migration

---
+++ ikCore/DataAccess/Migrations/202507231348052_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿namespace DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Companies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 200),
+                        TaxNumber = c.String(maxLength: 100),
+                        Address = c.String(maxLength: 200),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Email = c.String(nullable: false, maxLength: 150),
+                        PasswordHash = c.String(nullable: false),
+                        FullName = c.String(nullable: false, maxLength: 100),
+                        CompanyId = c.Int(nullable: false),
+                        IsEmailConfirmed = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Companies", t => t.CompanyId)
+                .Index(t => t.CompanyId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Users", "CompanyId", "dbo.Companies");
+            DropIndex("dbo.Users", new[] { "CompanyId" });
+            DropTable("dbo.Users");
+            DropTable("dbo.Companies");
+        }
+    }
+}
---
+++ ikCore/DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ikCore.DataAccess.Context.AppDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ikCore.DataAccess.Context.AppDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
