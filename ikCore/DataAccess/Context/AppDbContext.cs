b3ad16b27be581aba6f3b8a44dd68e6e41b352a1
Berkay Ozder <berkayq3535@gmail.com>
2025-07-23 16:01:54 +0300

    N-tier Architecture and Packages

---
+++ ikCore/DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext() : base("name=DefaultConnection") { }
+
+    //public DbSet<User> Users { get; set; }
+    //public DbSet<Company> Companies { get; set; }
+    // Diğer DbSet'ler eklenecek...
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // Fluent API ayarları buraya
+    }
+}
+
