617d9539e95153899692df39e0a097dad4d4720f
Berkay Ozder <berkayq3535@gmail.com>
2025-07-24 16:12:20 +0300

    BugFix

---
+++ Entities/Concrete/AdvanceRequest.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Base;
+using Entities.Interfaces;
+using System;
+
+
+namespace Entities.Concrete
+{
+    public class AdvanceRequest : BaseEntity, IEntity
+    {
+        public int EmployeeId { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; } // TRY, USD, vs.
+        public string Description { get; set; }
+        public DateTime RequestDate { get; set; }
+        public bool IsApproved { get; set; }
+        public DateTime? ApprovedDate { get; set; }
+        public int? ApprovedByUserId { get; set; }
+
+        // Navigation property
+        public virtual Employee Employee { get; set; }
+    }
+}
